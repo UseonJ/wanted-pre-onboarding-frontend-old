9e34ec9541b5310ea61c0791b2be714625fa008c
UseonJ <naul630@gmail.com>
2023-04-07 15:20:52 +0900

    ✨Create Message_div Component

---
+++ wanted-pre-onboarding-frontend/src/components/Message_div.js
@@ -0,0 +1,13 @@
+import {stc} from 'styled-components'
+
+const Message_div = stc.div`
+    color: var(--peach-600);
+    font-size: 12px;
+    padding-left: 20px;
+`
+
+export function Message_div () {
+    return (
+        <div></div>
+    )
+}
