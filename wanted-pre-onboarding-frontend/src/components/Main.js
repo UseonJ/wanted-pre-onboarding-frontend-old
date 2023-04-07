950e664df1697b6e19541e54fbaef47ea501c0cc
UseonJ <naul630@gmail.com>
2023-04-08 00:37:15 +0900

    💄 Main coponent remane classname
    for adjusting module.css

--- wanted-pre-onboarding-frontend/src/components/Main.js
+++ wanted-pre-onboarding-frontend/src/components/Main.js
@@ -1,13 +1,44 @@
-import stc from 'styled-components'
+// import stc from 'styled-components'
 
-const Main_C = stc.main`
-    border: none;
-    width: 12rem;
-    font-size: 14px;
-`
+// const Main_C = stc.main`
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+// padding: 24px;
+// box-shadow: var(--big-box);
+// border: 1px solid var(--gray-10);
+// border-radius: 4px;
+// `
+import styles from './Main.module.css';
 
 export function Main () {
     return (
-        <Main_C>test</Main_C>
+        <main className={styles.Main}> 
+        <fieldset>
+            <input type="text" className="username" placeholder="아이디" />
+        </fieldset>      
+        <div className={styles.hide}>사용할 수 있는 아이디입니다</div>
+        <div className={styles.hide}>아이디는 네 글자 이상이어야 합니다</div>      
+        <fieldset>
+            <input type="password" className="password" placeholder="비밀번호" />
+        </fieldset>
+
+      
+      <fieldset>
+        <input
+          type="password"
+          className="password-retype"
+          placeholder="비밀번호 확인"
+        />
+      </fieldset>
+
+      
+      <div className={styles.hide}>비밀번호가 일치하지 않습니다</div>
+
+      
+      <fieldset className={styles.signup}>
+        <button>회원가입</button>
+      </fieldset>
+        </main>
     )
 }
