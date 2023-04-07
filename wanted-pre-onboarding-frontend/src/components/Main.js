80c7f943efd4f550a1f6348cfdbb67324c539854
UseonJ <naul630@gmail.com>
2023-04-07 15:20:40 +0900

    ✨Create Main Component

---
+++ wanted-pre-onboarding-frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+import {stc} from 'styled-components'
+
+const Main = stc.main`
+    border: none;
+    width: 12rem;
+    font-size: 14px;
+`
+
+export function Main () {
+    return (
+        <Main></Main>
+    )
+}
