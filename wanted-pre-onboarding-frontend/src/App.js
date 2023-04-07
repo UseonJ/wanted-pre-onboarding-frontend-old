369f0caf9cd682e8668dcad140047b942ce077bd
UseonJ <naul630@gmail.com>
2023-04-07 10:47:48 +0900

    ✨Routing path setup

--- wanted-pre-onboarding-frontend/src/App.js
+++ wanted-pre-onboarding-frontend/src/App.js
@@ -1,8 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Todo from './pages/Todo';
 
 
 function App() {
+
+  //JWT token 보유 여부에 따라 true || false
+  const [isLogin,setIsLogin] = useState(false);
+
   return (
+  <BrowserRouter>
+      <Routes>
+        <Route path='/' />
+        <Route path='/signup' />
+        <Route path='/signin' />
+        <Route path='/todo' />
+      </Routes>
+  </BrowserRouter>
     
   );
 }
