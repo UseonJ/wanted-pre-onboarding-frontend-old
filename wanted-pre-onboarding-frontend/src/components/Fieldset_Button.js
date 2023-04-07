86fca4bceeda3a5f92def9417089c5279a396ced
UseonJ <naul630@gmail.com>
2023-04-07 22:58:25 +0900

    ✨ Fieldset Component setup

---
+++ wanted-pre-onboarding-frontend/src/components/Fieldset_Button.js
@@ -0,0 +1,10 @@
+import { Fieldset } from "./Fieldset"
+import { Button } from "./Button"
+
+export function Fieldset_Button () {
+    return (
+        <Fieldset>
+            <Button></Button>
+        </Fieldset>
+    )
+}
