b2d85dd730d5447e0dd61a4518f9e219abdf2526
UseonJ <naul630@gmail.com>
2023-04-07 15:20:30 +0900

    ✨Create Input Component

---
+++ wanted-pre-onboarding-frontend/src/components/Input.js
@@ -0,0 +1,13 @@
+import {stc} from 'styled-components'
+
+const Input = stc.input`
+    border: none;
+    width: 12rem;
+    font-size: 14px;
+`
+
+export function Input () {
+    return (
+        <input></input>
+    )
+}
