b4c01905a1f0ad40fdc5985adcd2793aea7c7864
UseonJ <naul630@gmail.com>
2023-04-08 16:50:00 +0900

    ♻️UI synchrounizing Signup & Singin page

--- wanted-pre-onboarding-frontend/src/pages/Signin.jsx
+++ wanted-pre-onboarding-frontend/src/pages/Signin.jsx
@@ -1,63 +1,31 @@
 import React, { useState } from 'react';
+import { Main} from '../components/Main';
+import stc from 'styled-components'
+
+const Signin_Body = stc.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+height: 100vh;
+background-image: linear-gradient(
+    to bottom,
+    rgba(69, 44, 221, 0.3),
+    rgba(255, 255, 255, 0)
+  )
+`
 
 export default function Signin(){
-    const [loginInfo, setLoginInfo] = useState({
-        userId: '',
-        password: '',
-      });
-      const [checkedKeepLogin, setCheckedKeepLogin] = useState(false);
-      const [errorMessage, setErrorMessage] = useState('');
-      const handleInputValue = (key) => (e) => {
-        setLoginInfo({ ...loginInfo, [key]: e.target.value });
-      };
-      const loginRequestHandler = () => {
-        const { userId, password } = loginInfo;
-        if (!userId || !password) {
-          setErrorMessage('아이디와 비밀번호를 입력하세요');
-          return;
-        } else {
-          setErrorMessage('');
-        }
-      };
+  
+  const title = '로그인'
+  
+  return(
+    
 
-    return(
-        <div className='container'>
-      <div className='left-box'>
-        <span>
-          Wanted
-          <p>Pre-onboarding</p>
-          Internship
-        </span>
-      </div>
-      <div className='right-box'>
-        <h1>AUTH STATES</h1>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <div className='input-field'>
-            <span>ID</span>
-            <input type='text' data-testid='id-input' onChange={handleInputValue('userId')} />
-            <span>Password</span>
-            <input
-              type='password'
-              data-testid='password-input'
-              onChange={handleInputValue('password')}
-            />
-            <label className='checkbox-container'>
-              <input type='checkbox' onChange={() => setCheckedKeepLogin(!checkedKeepLogin)} />
-              {' 로그인 상태 유지하기'}
-            </label>
-          </div>
-          <button type='submit' onClick={loginRequestHandler}>
-            LOGIN
-          </button>
-          {errorMessage ? (
-            <div id='alert-message' data-testid='alert-message'>
-              {errorMessage}
-            </div>
-          ) : (
-            ''
-          )}
-        </form>
-      </div>
-    </div>
+    <Signin_Body>
+      <Main title = {title}>      
+      </Main>
+    </Signin_Body>
+    
     )
 }
--- wanted-pre-onboarding-frontend/src/pages/Signup.jsx
+++ wanted-pre-onboarding-frontend/src/pages/Signup.jsx
@@ -1,8 +1,4 @@
 import stc from 'styled-components'
-
-import { Fieldset_Input } from "../components/Fieldset_Input";
-import { Fieldset_Button } from "../components/Fieldset_Button";
-import { Message_div } from "../components/Message_div";
 import { Main } from "../components/Main";
 
 const Signup_Body = stc.div`
@@ -19,21 +15,17 @@ background-image: linear-gradient(
 `
 
 export default function Signup () {
-    return(
+  
+  const title = '회원가입'
+
+  return(
+    
     
 
     <Signup_Body>
-      <Main>      
+      <Main title = {title}>      
       </Main>
     </Signup_Body>
     
     )
 }
-
-{/* <Fieldset_Input></Fieldset_Input>   */}
-{/* <Message_div></Message_div>
-      <Message_div></Message_div> */}
-{/* <Fieldset_Input></Fieldset_Input> */}
-{/* <Fieldset_Input></Fieldset_Input> */}
-{/* <Message_div></Message_div> */}
-{/* <Fieldset_Button></Fieldset_Button> */}
