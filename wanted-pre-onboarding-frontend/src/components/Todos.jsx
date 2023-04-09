420028512f5f69bb444c8e36d78e55730480edd9
UseonJ <naul630@gmail.com>
2023-04-09 09:38:09 +0900

    ✨todo page element setup

--- wanted-pre-onboarding-frontend/src/components/Todos.jsx
+++ wanted-pre-onboarding-frontend/src/components/Todos.jsx
@@ -3,10 +3,20 @@ import styles from './Todos.module.css';
 
 export function Todos () {
 
+
+
+
     return (
         <div className={styles.wrap}>
+          <header className={styles.titlecontainer}>
           <h1 className={styles.title}>To do List</h1>
-          <div className={styles.container}>
+          </header>
+          <div className={styles.createContainer}>
+            <input data-testid="new-todo-input"
+                   placeholder='할 일을 입력하세요' />
+            <button data-testid="new-todo-add-button">추가</button>
+          </div>
+          <div className={styles.listcontainer}>
             <div className={styles.list}>
             
             </div>
@@ -20,11 +30,15 @@ export function Todos () {
   <label>
     <input type="checkbox" />
     <span>TODO 1</span>
+    <button data-testid="modify-button">수정</button>
+    <button data-testid="delete-button">삭제</button>
   </label>
 </li>
 <li>
   <label>
     <input type="checkbox" />
     <span>TODO 2</span>
+    <button data-testid="modify-button">수정</button>
+    <button data-testid="delete-button">삭제</button>
   </label>
 </li> */}
